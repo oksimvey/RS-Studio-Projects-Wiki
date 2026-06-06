c2d71fe8bc06085ead8a9f6dc67a0c05171aad82
Marco <oksimvey@gmail.com>
2026-06-06 13:23:02 -0300

    upda

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/rs-studio-projects-wiki/",
+    base: "/RS-Studio-Projects-Wiki/",
   plugins: [react()],
 })
